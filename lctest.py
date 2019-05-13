a2a19ce7e70b5675e9e20a0b1d318630576b39de
Dennis Durling <djdtahoe@gmail.com>
2019-05-13 14:41:24 -0700

    push

--- lctest.py
+++
@@ -1,7 +0,0 @@
-import libcloud
-from libcloud import security
-security.VERIFY_SSL_CERT = False
-cls=libcloud.get_driver(type=libcloud.DriverType.COMPUTE, provider=libcloud.compute.types.Provider.HOSTVIRTUAL)
-with open('/home/dennis/api_key.txt', 'r') as f:
-    api_key = f.read().rstrip()
-conn=cls(api_key, api_version='1.0')
